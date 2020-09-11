4e296d6104f22440a3d2a52966996b747b417394
samueltiokeng <samueledward40@gmail.com>
2020-09-11 15:30:07 +0200

    s

--- views/public/script.js
+++ views/public/script.js
@@ -4,7 +4,7 @@ console.log(videoGrid);
 const myVideo = document.createElement('video');
 myVideo.muted = true;
 
-let myVideoStream;a
+let myVideoStream;
 navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true
