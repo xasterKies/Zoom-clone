a19250df0ac980717ee89d7e8953d3455ddc3c3f
samueltiokeng <samueledward40@gmail.com>
2020-09-11 13:13:15 +0200

    v

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 
+
+app.set(')view engine', 'ejs');
+
 app.get('/', (req, res) => {
     res.render('room');
 })
 
-app.set['view engine', 'ejs'];
+
 
 
 
