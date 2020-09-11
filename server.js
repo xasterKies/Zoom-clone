e48db3d9c85aa9e0d10322b9f16a52190e922f31
samueltiokeng <samueledward40@gmail.com>
2020-09-11 23:43:49 +0200

    socket.io

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+
 const {v4: uuidv4} = require('uuid');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
