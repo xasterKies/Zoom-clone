280f25a0b81d14266ace19f6b77ff5f501051f8e
samueltiokeng <samueledward40@gmail.com>
2020-09-12 03:02:07 +0200

    peer

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const {v4: uuidv4} = require('uuid');
 const { ExpressPeerSever } = require('peer');
-const
+const peerServer = ExpressPeerSever(server, {
+    debug: true
+});
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
