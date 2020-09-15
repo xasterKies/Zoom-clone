4916d0195ca72f76b5cf6bb1e183727763a6fbdb
samueltiokeng <samueledward40@gmail.com>
2020-09-15 07:14:51 +0200

    s

--- public/script.js
+++ public/script.js
@@ -55,7 +55,7 @@ const addVideoStream = (video, stream) => {
     videoGrid.append(video);
 }
 
-let msg = $('input');
+let text = $('input');
 
 $('html').keydown((e) => {
     if ( e.which== 13 && tex.val().length !==0) {
