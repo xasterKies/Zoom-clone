47e48feeccc4bcbe3bb05cf840302efac23ff7f0
samueltiokeng <samueledward40@gmail.com>
2020-09-15 12:34:45 +0200

    s

--- public/script.js
+++ public/script.js
@@ -125,7 +125,7 @@ const setStopVideo = () => {
 
 const setPlayVideo = () => {
     const html = `
-    <i class="stop fas fa-video-slash"></i>
+    <i class="stop fa fa-video-camera-slash"></i>
     <span>Play Video</span>
     `
     document.querySelector('.main__video__button').innerHTML = html;
